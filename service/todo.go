ad3bed26b593f025e1f0494ae920544b7ad30b94
kengo78 <fulken35@gmail.com>
2023-03-13 22:34:54 +0900

    update:

--- service/todo.go
+++ service/todo.go
@@ -25,8 +25,33 @@ func (s *TODOService) CreateTODO(ctx context.Context, subject, description strin
 		insert  = `INSERT INTO todos(subject, description) VALUES(?, ?)`
 		confirm = `SELECT subject, description, created_at, updated_at FROM todos WHERE id = ?`
 	)
+	// 「INSERT INTO テーブル名 (カラム1, カラム2, ...) VALUES (?, ?, ...)」のように定義します。ここでは、subject と description を保存するため、カラムはそれぞれの名前を指定し、値の代わりに ? を指定します。これは後から設定するためのプレースホルダーとして利用します。
+// 	次に、ExecContext メソッドを利用して SQL 文を実行します。ExecContext メソッドは、引数に渡された SQL 文を実行し、実行結果を返します。Insert 文の場合は、実行結果として ID を返すため、変数 result に代入します。
 
-	return nil, nil
+// その後、result.LastInsertId() メソッドを利用して、保存した TODO の ID を取得します。
+
+// 最後に、QueryRowContext メソッドを利用して、保存した TODO を取得し、model.TODO 型のインスタンスを作成して戻り値として返します。Scan メソッドを利用して、取得したデータを model.TODO 型のインスタンスに設定します。このメソッドは、Scan(dest ...interface{}) error のように引数にデータを設定する先のポインタを指定します。今回の場合は、todo の各フィールドを指定します。
+
+// なお、サブミットする前に、実際に動作確認するためには、依存パッケージのインストールや DB の準備が必要です
+	result, err := s.db.ExecContext(ctx, insert, subject, description)
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	row := s.db.QueryRowContext(ctx, confirm, id)
+	todo := &model.TODO{}
+	err = row.Scan(&todo.ID, &todo.Subject, &todo.Description, &todo.CreatedAt, &todo.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return todo, nil
+	// return nil, nil
 }
 
 // ReadTODO reads TODOs on DB.
